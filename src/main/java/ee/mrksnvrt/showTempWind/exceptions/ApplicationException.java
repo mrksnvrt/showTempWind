eeb41717a15fd55edacad3ad448492e4c9f753d7
Marek Siinvert <marek.siinvert@gmail.com>
2021-05-25 12:12:36 +0300

    Add exception handler

---
+++ src/main/java/ee/mrksnvrt/showTempWind/exceptions/ApplicationException.java
@@ -0,0 +1,8 @@
+package ee.mrksnvrt.showTempWind.exceptions;
+
+public class ApplicationException extends RuntimeException {
+
+    public ApplicationException(String message)
+    { super(message); }
+
+}
